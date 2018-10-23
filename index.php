f2de5d137c8a2f00302535990df9c3b60da9b4e3
devinrowell <devrow97@gmail.com>
2018-10-23 16:41:32 -0400

    added index page

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TerpMatch</title>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap stuff below -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+  <div id="main-content">
+    <nav class="navbar navbar-expand-sm bg-semiTrans">
+      <h4 class="navbar-brand logo">TerpMatch&#x1F422;</h4>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-link">
+          <a class="btn bg-kale" href="#">Login</a>
+        </li>
+        <li class="nav-link">
+          <a class="btn bg-kale" href="#">Register</a>
+        </li>
+      </ul>
+    </nav>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8 bg-kale-trans">
+          <h2 class="pt-1 text-center">Welcome to <span class="logo">TerpMatch&#x1F422;</span></h2>
+          
+        </div>
+        <div class="col-2"></div>
+      </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </div>
+</body>
+
+</html>
