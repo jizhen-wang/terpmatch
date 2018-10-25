63cf1a05fee139b7afb8f0d2e959ff301697c676
devinrowell <devrow97@gmail.com>
2018-10-25 16:42:54 -0400

    Finished index page layout

--- index.php
+++ index.php
@@ -22,40 +22,132 @@
 </head>
 
 <body>
-  <div id="main-content">
-    <nav class="navbar navbar-expand-sm bg-semiTrans">
-      <h4 class="navbar-brand logo">TerpMatch&#x1F422;</h4>
+  <div>
+    <nav class="navbar navbar-expand-sm bg-dark">
+      <small class="pr-2 text-white">Powered by</small>
+      <img class="navbar-brand nav-height" src="umd-logo-trans.jpg"/>
       <ul class="navbar-nav ml-auto">
         <li class="nav-link">
-          <a class="btn bg-kale" href="#">Login</a>
+          <a class="btn bg-terps-gold text-black black-hover" href="#">Login</a>
         </li>
         <li class="nav-link">
-          <a class="btn bg-kale" href="#">Register</a>
+          <a class="btn bg-terps-gold text-black black-hover" href="#">Register</a>
         </li>
       </ul>
     </nav>
-    <br />
-    <br />
-    <br />
-    <br />
-    <div class="container">
-      <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8 bg-kale-trans">
-          <h2 class="pt-1 text-center">Welcome to <span class="logo">TerpMatch&#x1F422;</span></h2>
-          
+    <div class="jumbotron" id="main-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8 bg-faded-yellow jumbo-margin rounded">
+            <h2 class="pt-2 text-center display-4">Welcome to <span class="logo">TerpMatch&#x1F422;</span></h2>
+            <p class="ml-2">
+              Whether you're looking for friends, study mates, or a significant other,
+              TerpMatch has you covered.
+            </p>
+            <ul>
+              <li>
+                Free to use.
+              </li>
+              <li>
+                Find Terps with similar interests, ideas, and goals as you.
+              </li>
+              <li>
+                Define the type of relationship you are looking for.
+              </li>
+              <li>
+                Direct message Terps that you match with.
+              </li>
+              <li>
+                Give feedback on your matches to help us tune future matches.
+              </li>
+            </ul>
+            <ul>
+              <li class="standout-point">
+                <strong>Become connected with Terps like never before.</strong>
+              </li>
+            </ul>
+            <div class="col-12 text-center pb-3">
+              <a class="btn btn-lg bg-terps-red text-white" href="#">Register Now</a>
+            </div>
+          </div>
+          <div class="col-2"></div>
         </div>
-        <div class="col-2"></div>
       </div>
     </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col-4 text-center">
+          <blockquote class="blockquote">
+            <p>
+              I met not only my first wife, but my next ten wives all on TerpMatch!
+              It's the best for UMD students.
+            </p>
+          </blockquote>
+          <em>- Finn Denhard</em>
+        </div>
+        <div class="col-4 text-center">
+          <blockquote class="blockquote">
+            <p>
+              I was looking for someone to study with, but instead I found the
+              love of my life! Thanks TerpMatch!
+            </p>
+          </blockquote>
+          <em>- Fonn Donhord</em>
+        </div>
+        <div class="col-4 text-center">
+          <blockquote class="blockquote">
+            <p>
+              I like turtles way more than the average person, so TerpMatch was
+              the perfect site for me.
+            </p>
+          </blockquote>
+          <em>- Fann Dinhurd</em>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-dark mt-5 pb-1 pt-3">
+    <h3 class="text-center text-white display-4">4 Easy Steps to Find the Perfect Terp</h3>
+    <div class="card-deck pt-3 px-5 pb-3">
+      <div class="card bg-terps-gold">
+        <div class="card-body text-center">
+          <h4 class="card-title">&#9312; Register Here</h4>
+          <p class="card-text">
+            Sign up for TerpMatch by creating an account. It's as easy as
+            choosing a username and password. No payment. No strings attached.
+          </p>
+        </div>
+      </div>
+      <div class="card bg-terps-gold">
+        <div class="card-body text-center">
+          <h4 class="card-title">&#9313; Fill out info</h4>
+          <p class="card-text">
+            Help us find you the perfect TerpMatch by providing us with information
+            about yourself and your interests. We use this info solely for finding
+            you matches, not nefarious purposes.
+          </p>
+        </div>
+      </div>
+      <div class="card bg-terps-gold">
+        <div class="card-body text-center">
+          <h4 class="card-title">&#9314; Generate matches</h4>
+          <p class="card-text">
+            You choose when to look for matches. We make it easy to sort through
+            your matches so that you can generate new ones whenever you like.
+          </p>
+        </div>
+      </div>
+      <div class="card bg-terps-gold">
+        <div class="card-body text-center">
+          <h4 class="card-title">&#9315; Give feedback</h4>
+          <p class="card-text">
+            Once you have seen and interacted with a TerpMatch, you can rate
+            how we did as well as what you though of your match.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </body>
 
