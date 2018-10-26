1cebc30d1e7711bf24a920e9c0146f30f698a519
devinrowell <devrow97@gmail.com>
2018-10-26 17:18:34 -0400

    changed style of login modal slightly

--- index.php
+++ index.php
@@ -217,39 +217,31 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Login</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <!-- <div class="modal-header">
+        <h4 class="modal-title">Login</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div> -->
+
+      <!-- Modal body -->
+      <form>
+        <div class="modal-body">
+          <h3 class="modal-title py-3 text-center">Login to TerpMatch</h3>
+          <div class="form-group">
+            <input class="form-control" type="text" name="username" id="username" placeholder="Username">
+          </div>
+          <div class="form-group">
+            <input class="form-control" type="password" name="password" id="pwd" placeholder="Password">
+          </div>
+          <input type="submit" class="btn bg-terps-red btn-block" data-dismiss="modal" value="Login">
         </div>
 
-        <!-- Modal body -->
-        <form>
-          <div class="modal-body">
-            <div class="form-row">
-              <div class="form-group col-sm-4">
-                <label>Username:
-                  <input class="form-control" type="text" name="username">
-                </label>
-              </div>
-            </div>
-            <div class="form-row">
-              <div class="form-group col-sm-4">
-                <label>Password:
-                  <input class="form-control" type="password" name="password">
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <!-- Modal footer -->
-          <div class="modal-footer">
-            <input type="submit" class="btn btn-danger" data-dismiss="modal" value="Login">
-          </div>
-        </form>
-
-      </div>
+        <!-- Modal footer -->
+        <div class="modal-footer">
+        </div>
+      </form>
     </div>
   </div>
+</div>
 
 </body>
 
