f35f8be87737b6e198573fc84c2b45c288d32536
jzwang43 <jzwang43@gmail.com>
2018-10-26 14:54:30 -0400

    Created modals in main page.

--- register.php
+++ register.php
@@ -27,7 +27,9 @@
         <h1 class="navbar-brand logo">TerpMatch&#x1F422;</h1>
         <ul class="navbar-nav ml-auto">
             <li class="nav-link">
-                <a class="btn bg-kale" href="#">Login</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                    Open modal
+                </button>
             </li>
         </ul>
     </nav>
@@ -85,6 +87,30 @@
     <br>
     <br>
     <br>
+</div>
+
+<div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Modal Heading</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal body -->
+            <div class="modal-body">
+                Modal body..
+            </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+
+        </div>
+    </div>
 </div>
 </body>
 
