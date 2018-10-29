09debfa5ce8fe19f4dcdc8292676375edd447443
Yufei <yufeih19@terpmail.umd.edu>
2018-10-29 01:59:10 -0400

    Added registration.php, WIP.

---
+++ registration.php
@@ -0,0 +1,35 @@
+<?php
+/* Yufei Huang 10/29/2018 */
+
+require_once "dblogin.php";
+session_start();
+
+$db_connection=new mysqli($host, $user, $db_password, $database);
+if ($db_connection->connect_error) {
+    die($db_connection->connect_error);
+}
+
+$username = trim($_POST['username']);
+$password = $_POST['password'];
+$first_name = trim($_POST['first_name']);
+$middle_name = trim($_POST['middle_name']);
+$last_name = trim($_POST['last_name']);
+$gender = $_POST['gender'];
+$bd = $_POST['birthday'];
+$year_in_school = $_POST['year_in_school'];
+$major = $_POST['major'];
+
+$sql = sprintf("INSERT INTO accounts VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", $username, $password, $first_name, $middle_name, $last_name, $gender, $bd, $year_in_school, $major); 
+echo $sql;
+$result = mysqli_query($db_connection, $sql);
+
+if ($result) {
+    echo "hey it works";
+}
+else {
+    echo "oh it fails";
+    echo $result;
+    echo mysqli_error($db_connection);
+}
+    
+?>
