41d793ae3466df4b1f4dece731e5322c64e8d666
devinrowell <devrow97@gmail.com>
2018-11-26 22:04:21 -0500

    working on photos

--- js/matches.js
+++ js/matches.js
@@ -1,7 +1,28 @@
+// <div class="card shadow">
+//     <div class="card-body">
+//         <img src="img/default.jpg" alt="profile picture" class="card-img-top rounded-circle"/>
+//         <h4 class="pt-3">Match One</h4>
+//         <p>
+//             Bio text goes here. This is the same as the bio text that the
+//             match sets.
+//         </p>
+//         <p class="text-center">
+//             <b>Score:</b> XX
+//         </p>
+//     </div>
+// </div>
+
+
+function displayCard() {
+  let str = "<div class='card-deck'>";
+  str += "div class='card-shadow'>";
+  str += "div class='card-body'>";
+}
 
 function generateMatch() {
   $.get('services/findmatch.php', function (data) {
-    $('#matches-deck').append(data);
+    let matchResult = JSON.parse(data)[0];
+    displayCard(matchResult);
   });
 }
 
